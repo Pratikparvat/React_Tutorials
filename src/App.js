0ec28a5b62dc4849a8c3669554c9e096dea6749e
Pratikparvat <pratikparvat@gmail.com>
2024-04-16 22:58:44 +0530

    new additions

--- src/App.js
+++ src/App.js
@@ -3,16 +3,21 @@ import './App.css';
 import Greet from './Components/Greet'
 import Welcome from './Components/Welcome';
 import Hello from './Components/Hello';
+import Message from './Components/Message';
 
 function App() {
   return (
     <div className="App">
-      <Greet name="Pratik"/>
-      <Greet name="Ashish"/>
+      <Message/>
+      {/* <Greet name="Pratik"/>
+      <Greet name="Ashish">
+        <p>Childern prop</p>
+        <button>Action</button>
+      </Greet>
       <Greet name="XYZ"/>
 
       <Welcome name="dynamic"/>
-      <Hello/>
+      <Hello/> */}
     </div>
   );
 }
